74905e526620f6888515292b207ddbfad26a3618
Umar Nazir <erumarnazir@gmail.com>
2026-09-05 00:28:10 +0530

    Change hostname to production

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { parse } = require('url');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
+const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 const app = next({ dev, hostname, port });
